e86dca43511ba57c5b9487e7ab34b5fef1ef801f
OmarYZY <omaramirez134@gmail.com>
2026-02-26 11:34:45 -0600

    Se añadieron las rutas que se crearon en auth.js,profile.js y menu.js

--- annie-backend/server.js
+++ annie-backend/server.js
@@ -1,17 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
-require('dotenv').config();
+const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
+const menuRoutes = require('./routes/menu');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Conectar a MongoDB
-connectDB();
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send("Bienvenido a Annie Backend 🚀");
-});
-
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+app.use('/menu', menuRoutes);
