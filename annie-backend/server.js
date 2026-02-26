d2251bcb3763326e2c16679da77c72b775dd7202
OmarYZY <omaramirez134@gmail.com>
2026-02-26 10:40:51 -0600

    Se agrego la conexion en server.js

--- annie-backend/server.js
+++ annie-backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Conectar a MongoDB
+connectDB();
 
 app.use(express.json());
 
