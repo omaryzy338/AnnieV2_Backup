64978752501f8f9ac08f44df262226c93f3e9d8d
OmarYZY <omaramirez134@gmail.com>
2026-03-18 12:33:45 -0600

    Modifique el classname

--- annie-frontend/src/pages/Login.jsx
+++ annie-frontend/src/pages/Login.jsx
@@ -52,7 +52,7 @@ const Login = () => {
             required
           />
         </div>
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="btn btn-success">
           Entrar
         </button>
       </form>
