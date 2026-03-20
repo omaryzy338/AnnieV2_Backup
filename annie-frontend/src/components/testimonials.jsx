e004db7065032e94703a0bc4729ca20c73f43178
OmarYZY <omaramirez134@gmail.com>
2026-03-20 11:39:39 -0600

    Modifique textos

--- annie-frontend/src/components/testimonials.jsx
+++ annie-frontend/src/components/testimonials.jsx
@@ -5,7 +5,7 @@ export const Testimonials = (props) => {
     <div id="testimonials">
       <div className="container">
         <div className="section-title text-center">
-          <h2>El Problema Actual</h2>
+          <h2>Testimonios de Nuestros Clientes</h2>
         </div>
         <div className="row">
           {props.data
