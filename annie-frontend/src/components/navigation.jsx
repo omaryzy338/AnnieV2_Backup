9436157223f66d6205bc4d5c22c4e8a827ea1934
Azel <axelarmandoramirezcerda@gmail.com>
2026-03-21 20:23:42 -0600

    logo en navegacion

--- annie-frontend/src/components/navigation.jsx
+++ annie-frontend/src/components/navigation.jsx
@@ -31,11 +31,14 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>
           </button>
           <Link className="navbar-brand page-scroll" to="/" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-            <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="32" height="32" rx="6" fill="#6372ff"/>
-              <text x="16" y="22" fontSize="18" fontWeight="bold" fill="white" textAnchor="middle" fontFamily="Arial">A</text>
-            </svg>
-            <span style={{ fontWeight: "600", fontSize: "18px" }}>ANNIE</span>
+
+            <img 
+    src="/logo512.png" 
+    alt="Logo ANNIE" 
+    width="200" 
+    height="200" 
+    style={{ marginRight: "auto", marginLeft: "auto", marginTop: "10px" }}
+  />
           </Link>
         </div>
 
