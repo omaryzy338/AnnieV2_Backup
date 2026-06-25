cb4e60fc3b62a58f9c87744df66c9a59dbdc7236
Angel <22736@virtual.utsc.edu.mx>
2026-06-24 18:49:21 -0600

    Cambio de la barra de navegación

--- annie-frontend/src/components/navigation.jsx
+++ annie-frontend/src/components/navigation.jsx
@@ -17,10 +17,18 @@ export const Navigation = (props) => {
     window.location.reload();
   };
 
-  return (
-    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
+  const navItems = [
+    { href: "/", icon: "logo", label: "Inicio", external: false },
+    { href: "#about", icon: "fa fa-lightbulb-o", label: "Propuesta", external: true },
+    { href: "#services", icon: "fa fa-cogs", label: "Funciones", external: true },
+    { href: "#portfolio", icon: "fa fa-code", label: "Tecnología", external: true },
+    { href: "#testimonials", icon: "fa fa-comments", label: "Voces", external: true },
+    { href: "#contact", icon: "fa fa-envelope", label: "Contacto", external: true },
+  ];
 
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top annie-nav">
+      <div className="container">
         <div className="navbar-header">
           <button
             type="button"
@@ -33,74 +41,62 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>
             <span className="icon-bar"></span>
           </button>
-
-          <Link
-            className="navbar-brand page-scroll"
-            to="/"
-            style={{ display: "flex", alignItems: "center", gap: "8px" }}
-          >
-                              <img
-                                src="/favicon.png"
-                                alt="Logo ANNIE"
-                                style={{ height: "75px", width: "auto", display: "block", margin: 0 }}
-                              />
-
-          </Link>
         </div>
 
-        <div
-          className="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1"
-        >
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="#features" className="page-scroll">
-                Desafios
-              </a>
-            </li>
-            <li>
-              <a href="#about" className="page-scroll">
-                que somos
-              </a>
-            </li>
-            <li>
-              <a href="#services" className="page-scroll">
-                Funcionalidades
-              </a>
-            </li>
-            <li>
-              <a href="#portfolio" className="page-scroll">
-                Tecnologias
-              </a>
-            </li>
-            <li>
-              <a href="#testimonials" className="page-scroll">
-                Testimonios
-              </a>
-            </li>
-            <li>
-              <a href="#contact" className="page-scroll">
-                Contact
-              </a>
-            </li>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right annie-nav__links">
+            {navItems.map((item) => (
+              <li key={item.label}>
+                {item.external ? (
+                  <a href={item.href} className="page-scroll annie-nav__link">
+                    {item.icon === "logo" ? (
+                      <img src="/favicon.png" alt="ANNIE" className="annie-nav__item-logo" />
+                    ) : (
+                      <i className={item.icon} aria-hidden="true" />
+                    )}
+                    <span>{item.label}</span>
+                  </a>
+                ) : (
+                  <Link to={item.href} className="annie-nav__link">
+                    {item.icon === "logo" ? (
+                      <img src="/favicon.png" alt="ANNIE" className="annie-nav__item-logo" />
+                    ) : (
+                      <i className={item.icon} aria-hidden="true" />
+                    )}
+                    <span>{item.label}</span>
+                  </Link>
+                )}
+              </li>
+            ))}
+
             {isLogged ? (
               <li>
-                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/dashboard" className="annie-nav__cta annie-nav__link">
+                  <i className="fa fa-th-large" aria-hidden="true" />
+                  <span>Mi panel</span>
+                </Link>
               </li>
             ) : (
               <>
                 <li>
-                  <Link to="/login">Login</Link>
+                  <Link to="/login" className="annie-nav__link">
+                    <i className="fa fa-sign-in" aria-hidden="true" />
+                    <span>Entrar</span>
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/register">Register</Link>
+                  <Link to="/register" className="annie-nav__link annie-nav__cta-link">
+                    <span>Crear cuenta</span>
+                  </Link>
                 </li>
               </>
             )}
+
             {isLogged && (
               <li>
-                <a onClick={handleLogout} style={{ cursor: "pointer" }}>
-                  Logout
+                <a onClick={handleLogout} className="annie-nav__link" style={{ cursor: "pointer" }}>
+                  <i className="fa fa-sign-out" aria-hidden="true" />
+                  <span>Salir</span>
                 </a>
               </li>
             )}
